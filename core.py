6f59db7c2eb0f52b93c760c7838550cc0958f5ba
oak <oakery@qq.com>
2022-07-19 09:10:14 +0800

    STRCT: Adjust the structure of database

--- core.py
+++ core.py
@@ -1,4 +1,85 @@
-import sqlalchemy
+import datetime
+import quool as ql
+import pandas as pd
+import sqlalchemy as sql
+
+
+class DataBase:
+    def __init__(self, database: sql.engine.Engine) -> None:
+        self.today = datetime.datetime.today()
+        self.engine = database
+        
+    def __query(self, table: str, start: str, end: str, date_col: str, 
+        code: 'str | list', code_col: str, fields: 'list | str', 
+        index_col: 'str | list', and_: 'list | str', or_: 'str | list'):
+        start = start or '20070104'
+        end = end or self.today
+        start = ql.str2time(start)
+        end = ql.str2time(end)
+        code = ql.item2list(code)
+        fields = ql.item2list(fields)
+        index_col = ql.item2list(index_col)
+        and_ = ql.item2list(and_)
+        or_ = ql.item2list(or_)
+
+        if fields:
+            fields = set(fields).union(set(index_col))
+            fields = ','.join(fields)
+        else:
+            fields = '*'
+
+        query = f'select {fields} from {table}'
+        query += f' where ' if any([date_col, code, and_, or_]) else ''
+        
+        if date_col:
+            query += f' ( {date_col} between "{start}" and "{end}" )'
+            
+        if code:
+            query += ' and ' if any([date_col]) else ''
+            query += '(' + ' or '.join([f'{code_col} like "%%{c}%%"' for c in code]) + ')'
+        
+        if and_:
+            query += ' and ' if any([date_col, code]) else ''
+            query += '(' + ' and '.join(and_) + ')'
+        
+        if or_:
+            query += ' and ' if any([date_col, code, and_]) else ''
+            query += '(' + ' or '.join(or_) + ')'
+        
+        return query
+
+    def get(self, 
+        table: str,
+        start: str, 
+        end: str,
+        date_col: str,
+        code: 'str | list',
+        code_col: str,
+        fields: list, 
+        index_col: 'str | list',
+        and_: 'str | list', 
+        or_: 'str | list',
+        ) -> pd.DataFrame:
+        query = self.__query(
+            table,
+            start,
+            end,
+            date_col,
+            code,
+            code_col,
+            fields,
+            index_col,
+            and_,
+            or_
+        )
+        data = pd.read_sql(query, self.engine, parse_dates=date_col)
+        if index_col is not None:
+            data = data.set_index(index_col)
+        if fields is not None and isinstance(fields, str):
+            data = data.iloc[:, 0]
+        if isinstance(code, str):
+            data = data.droplevel(1)
+        return data
 
 
 class Loader:
@@ -8,7 +89,7 @@ class Loader:
         self.database = config['database']
         self.addindex = config.get('addindex', None)
         if isinstance(self.database, str):
-            self.database = sqlalchemy.engine.create_engine(self.database)
+            self.database = sql.engine.create_engine(self.database)
     
     def post(self):
         if self.addindex is not None:
--- staff/__init__.py
+++ staff/__init__.py
@@ -2,6 +2,7 @@ from .local import Local
 from .tushare import TuShare
 from .stockus import StockUS
 from .em import Em
+
 from .provider import (
     Stock,
     TuShare,
